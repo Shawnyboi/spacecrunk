5144436cbd355550cd341e6e107cc332c8b8ffd8
Sam Trapp <trapp.s.e@gmail.com>
2019-09-21 02:25:32 -0600

    Basic movement

---
+++ Assets/Scripts/CrunkMover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CrunkMover : MonoBehaviour
+{
+	Rigidbody body = null;
+	[SerializeField]
+	Vector3 horizontalAxis = Vector3.right;
+	[SerializeField]
+	Vector3 verticalAxis = Vector3.up;
+	[SerializeField]
+	float maxSpeed = 10;
+	[SerializeField]
+	float acceleration = 10;
+
+	bool grounded = true;
+
+	private void Start()
+	{
+		body = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	private void FixedUpdate()
+	{
+		var horizontal = Input.GetAxis("Horizontal");
+		var vertical = Input.GetAxis("Vertical");
+
+		var force = ((horizontalAxis * horizontal) + (verticalAxis * vertical)) * acceleration;
+		body.AddForce(force);
+
+		if (body.velocity.sqrMagnitude > (maxSpeed * maxSpeed))
+		{
+			float speedScale = maxSpeed / body.velocity.magnitude;
+			body.velocity *= speedScale;
+		}
+	}
+}
