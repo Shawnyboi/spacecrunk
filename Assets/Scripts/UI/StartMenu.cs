59188689e67ebd7014d8737e9dc430214102ef06
Sam Trapp <trapp.s.e@gmail.com>
2019-09-22 20:31:03 -0600

    hooking up start screen

---
+++ Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+	public void StartMatch()
+	{
+		SceneManager.LoadScene("2ships_____atthesametime");
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
