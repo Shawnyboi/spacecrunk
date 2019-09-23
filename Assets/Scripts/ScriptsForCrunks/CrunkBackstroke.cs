50f99cdb99cd158ac855fb191ea44cc99e382d8a
Sam Trapp <trapp.s.e@gmail.com>
2019-09-22 18:20:52 -0600

    Hooking up backstroke animation

---
+++ Assets/Scripts/ScriptsForCrunks/CrunkBackstroke.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrunkBackstroke : MonoBehaviour
+{
+	Crunk crunk = null;
+	CrunkAnimation crunkAnimation;
+
+	bool wasStroking = false;
+
+	private void Start()
+	{
+		crunk = GetComponent<Crunk>();
+		crunkAnimation = GetComponent<CrunkAnimation>();
+	}
+
+	private void Update()
+	{
+		bool stroking = false;
+		if (crunk.parentShip == null)
+		{
+			stroking = Input.GetAxis($"Charge{crunk.playerNumber}") > Helper.Epsilon;
+
+			if (stroking)
+			{
+				if (!wasStroking)
+				{
+					crunkAnimation.StartBackstroking();
+				}
+
+			}
+			else
+			{
+				if (wasStroking)
+				{
+					crunkAnimation.StopBackstroking();
+				}
+			}
+		}
+
+		wasStroking = stroking;
+	}
+}
